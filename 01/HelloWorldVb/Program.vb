c988fe39a37b1c8486b9b40e2f0b1d51b9acd36e
kaurrevature <33356218+kaurrevature@users.noreply.github.com>
2021-11-24 11:38:43 +0530

    Vb.net Hello world program

---
+++ 01/HelloWorldVb/Program.vb
@@ -0,0 +1,9 @@
+'predefined namespace
+Imports System
+
+'Module is like a class but it has only one instance
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+    End Sub
+End Module
